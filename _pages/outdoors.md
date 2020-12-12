11723878185ed9b9027c31fb238275a1f9bdf8e9
Robert Puccinelli <robertpuccinelli@users.noreply.github.com>
2020-12-12 14:41:50 -0800

    _site only

--- _pages/outdoors.md
+++
@@ -1,9 +0,0 @@
----
-title: The Outdoors
-layout: single
-permalink: /outdoors/
-
-header:
-  overlay_image: /assets/images/banners/OutdoorsBanner_PointLobos.jpg
-  caption: "Point Lobos State Park, 2016"
----
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Build Your Own: Gel Illumination and Imaging Chamber",
+        "excerpt":"PURPOSE: Provide a DIY alternative to expensive commercial gel imaging solutions. Benefits include exposing operators to fewer hazardous materials, reducing equipment cost and possibly improving image quality. It was initially designed for the Fordyce lab at Stanford in Dec 2014 and was further modified in July 2017 to improve the...","categories": ["build"],
+        "tags": ["build","imaging"],
+        "url": "https://robertpuccinelli.com/build/2017/07/02/build-gel-chamber.html",
+        "teaser": null
+      },{
+        "title": "Build Your Own: Microarray Printer",
+        "excerpt":"Small protein spots capture various fluorescent DNA sequences from a solubilized microarray. PURPOSE: Provide a DIY alternative to expensive commercial microarray solutions. Benefits of this system include high-throughput spotting and microarray customizability. It was initially built and documented by Joe DeRisi and team back in the late 90’s. One of...","categories": ["build"],
+        "tags": ["build","electronics"],
+        "url": "https://robertpuccinelli.com/build/2017/07/04/build-microarrayer.html",
+        "teaser": null
+      },{
+        "title": "A Modern Antibiotic Resistance Reporting System",
+        "excerpt":"Foreword: This project was conceived at the end of 2016 as I was considering solutions for meeting the various antibiotic challenges that were accepting applications at the time. Unfortunately, I was underprepared and didn’t submit an application while I could. Since then, the idea has continued to incubate in my...","categories": ["public health"],
+        "tags": ["antibiotic resistance project","commentary"],
+        "url": "https://robertpuccinelli.com/public%20health/2017/07/05/antibiotic-resistance-reporting.html",
+        "teaser": null
+      },{
+        "title": "Build Your Own: Automotive Head Unit",
+        "excerpt":"PURPOSE: Provide a DIY alternative to expensive commercial head units. The following in-depth guide is designed to allow anyone to rebuild their system in the event of a break-in or upgrade their system at their own discretion. I would like to thank my insurance company, AAA, for supporting this rebuild...","categories": ["build"],
+        "tags": ["build","jeep","electronics"],
+        "url": "https://robertpuccinelli.com/build/2019/01/21/updating-jeep-head-unit.html",
+        "teaser": null
+      },{
+        "title": "Resources for Embedded Development",
+        "excerpt":"Introduction Briefly, I found the following resources to be useful for embedded hardware development. More resources will be added over time. Microcontrollers The Amazing $1 Microcontroller A very informative and thorough review. Debugging Debugging Firmware with GDB A nice, short walkthrough for remote debugging. GDB Cheatsheet Tidy collection of GDB...","categories": ["tools"],
+        "tags": ["hardware","electronics"],
+        "url": "https://robertpuccinelli.com/tools/2019/05/21/embedded-resources.html",
+        "teaser": null
+      },{
+        "title": "SCI-Arc 3D Printer Recommendations and Services",
+        "excerpt":"PURPOSE: It can be a bit daunting to get started with 3D printing when there are so many options out there. On the other hand, if you have already spent some time printing, it could be fun to see what upgrades are out there to improve your printing experience. The...","categories": ["services"],
+        "tags": ["build","printing"],
+        "url": "https://robertpuccinelli.com/services/2019/09/03/3d-printer-recommendations.html",
+        "teaser": null
+      },{
+        "title": "Toastmasters: (0) Ice Breaker",
+        "excerpt":"Overview One’s first speech at Toastmasters is a ~5 minute ice breaker that is used to introduce yourself to the club. The speech presented here describes what happens to my mind when it is struck with social anxiety and how it impacts my interaction with others. The speech is formatted...","categories": ["toastmasters"],
+        "tags": ["speech"],
+        "url": "https://robertpuccinelli.com/toastmasters/2020/02/08/ice-breaker.html",
+        "teaser": null
+      }]
